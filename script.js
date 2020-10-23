5866c912a43535b51e5ecd5f29138d854993726e
Luiz Carlos Wagner <luizcarloswagner@gmail.com>
2020-10-23 10:55:15 -0300

    feat: add snapShotResize e melhora snapShot

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-import { loadCamera } from './modules/camera.js';
+import { loadCamera, snapShot, snapshotResize } from './modules/camera.js';
 import alert from './modules/alert.js';
+import removeNode from './modules/removeNode.js';
 
 const buttonLoad = document.getElementById('button-load');
-const buttonSnap = document.getElementById('button-snap');
+const buttonSnapShot = document.getElementById('button-snap');
 const elementVideo = document.getElementById('video');
+const resultSnapShot = document.getElementById('preview');
+const resultSnapShotAlt = document.getElementById('preview-alt');
 
-buttonLoad.addEventListener('click', (event) => {
+buttonLoad.addEventListener('click', () => {
   loadCamera(elementVideo, null, (error) => {
     const options = { id: 'alert', timeout: 5000 };
 
@@ -21,4 +24,21 @@ buttonLoad.addEventListener('click', (event) => {
         break;
     }
   });
+});
+
+buttonSnapShot.addEventListener('click', () => {
+  const image = document.createElement('img');
+  const imageAlt = document.createElement('img');
+  image.src = snapshotResize(elementVideo, 1920, 1080);
+  imageAlt.src = snapShot(elementVideo, 480, 400);
+
+  while (resultSnapShot.firstChild) {
+    removeNode(resultSnapShot.firstChild);
+  }
+  while (resultSnapShotAlt.firstChild) {
+    removeNode(resultSnapShotAlt.firstChild);
+  }
+
+  resultSnapShot.append(image);
+  resultSnapShotAlt.appendChild(imageAlt);
 });
